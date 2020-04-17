cecc8671301ae2418037607752b8d39210f6bfe7
Kamal Mahyuddin <kamal@envoy.com>
2020-04-16 18:39:13 -0700

    Add rollup plugin to maintain external automatically

--- rollup.config.js
+++ rollup.config.js
@@ -1,15 +1,19 @@
 import babel from "rollup-plugin-babel";
 import commonjs from "rollup-plugin-commonjs";
+import peerDepsExternal from "rollup-plugin-peer-deps-external";
 
 const formats = ["esm", "umd"];
-const plugins = [babel({ exclude: "node_modules/**" }), commonjs()];
+const plugins = [
+  peerDepsExternal(),
+  babel({ exclude: "node_modules/**" }),
+  commonjs(),
+];
 const globals = { react: "React" };
 
 export default [
   {
     input: "src/index.js",
     plugins,
-    external: ["react"],
     output: formats.map((format) => ({
       file: `dist/index.${format}.js`,
       format,
