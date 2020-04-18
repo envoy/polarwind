089f6b2ec96a3fe23268190a33caf9482d7c7a14
Kamal Mahyuddin <kamal@envoy.com>
2020-04-17 22:03:45 -0700

    Add propTypes to Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,14 @@
+import PropTypes from "prop-types";
 import styles from "./Button.module.css";
 import classNames from "classnames/bind";
 
 const cx = classNames.bind(styles);
 
+/**
+ * Buttons are used primarily for actions, such as "Add", "Close", "Cancel", or "Save".
+ * Plain buttons, which look similar to links, are used for less important or less
+ * commonly used actions, such as "view settings".
+ */
 export const Button = ({ children, disabled, size = "medium" }) => {
   const className = cx({
     Button: true,
@@ -17,4 +23,13 @@ export const Button = ({ children, disabled, size = "medium" }) => {
       {children}
     </button>
   );
+};
+
+Button.propTypes = {
+  /** The content to display inside the button */
+  children: PropTypes.string,
+  /** Disables the button, disallowing interaction */
+  disabled: PropTypes.bool,
+  /** Changes the size of the button, giving it more or less padding */
+  size: PropTypes.oneOf(["slim", "medium", "large"]),
 };
