5f7ca6905ffd3ebd5a18dc4d9098e989fa17fcd9
Kamal Mahyuddin <kamal@envoy.com>
2020-04-21 19:20:20 -0700

    Add success to text field

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -10,7 +10,14 @@ const cx = classNames.bind(styles);
  * A text field is an input field that users can type into. It has a range of options and
  * supports several text formats including numbers.
  */
-export const TextField = ({ error, label, required, type = "text" }) => {
+export const TextField = ({
+  error,
+  helpText,
+  label,
+  success,
+  required,
+  type = "text",
+}) => {
   const [focus, setFocus] = useState(false);
   const onFocus = () => {
     setFocus(true);
@@ -21,13 +28,18 @@ export const TextField = ({ error, label, required, type = "text" }) => {
 
   const labelMarkup = (id) => <label htmlFor={id}>{label}</label>;
 
+  const helpTextMarkup = helpText && <p>{helpText}</p>;
+  const successMarkup = success && <p>{success}</p>;
   const errorMarkup = error && <p>{error}</p>;
+
+  const isError = error && error.length > 1;
 
   const className = cx({
     TextField: true,
-    error: error && error.length > 1,
+    error: isError,
     focus,
     required,
+    success: !isError && success && success.length > 1,
   });
 
   return (
@@ -38,7 +50,7 @@ export const TextField = ({ error, label, required, type = "text" }) => {
           <div className="input-shadow">
             <input type={type} id={id} onFocus={onFocus} onBlur={onBlur} />
           </div>
-          {errorMarkup}
+          {errorMarkup || successMarkup || helpTextMarkup}
         </div>
       )}
     </UIDConsumer>
@@ -48,10 +60,14 @@ export const TextField = ({ error, label, required, type = "text" }) => {
 TextField.propTypes = {
   /** Error to display beneath the label */
   error: PropTypes.string,
+  /** Additional hint text to display */
+  helpText: PropTypes.string,
   /** Label for the input */
   label: PropTypes.string.isRequired,
   /** Mark the field as required */
   required: PropTypes.bool,
+  /** Success message to display beneath the label */
+  success: PropTypes.string,
   /** Determines the type of input */
   type: PropTypes.oneOf(["text"]),
 };
