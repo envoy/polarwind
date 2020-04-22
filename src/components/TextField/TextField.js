c782f62698ea60de82267a091a9f489a67fdffa9
Kamal Mahyuddin <kamal@envoy.com>
2020-04-21 18:38:51 -0700

    Add label for text field

--- src/components/TextField/TextField.js
+++ src/components/TextField/TextField.js
@@ -2,6 +2,7 @@ import { useState } from "react";
 import PropTypes from "prop-types";
 import styles from "./TextField.module.css";
 import classNames from "classnames/bind";
+import { UIDConsumer } from "react-uid";
 
 const cx = classNames.bind(styles);
 
@@ -9,7 +10,7 @@ const cx = classNames.bind(styles);
  * A text field is an input field that users can type into. It has a range of options and
  * supports several text formats including numbers.
  */
-export const TextField = ({ error, type = "text" }) => {
+export const TextField = ({ error, label, type = "text" }) => {
   const [focus, setFocus] = useState(false);
   const onFocus = () => {
     setFocus(true);
@@ -27,18 +28,25 @@ export const TextField = ({ error, type = "text" }) => {
   });
 
   return (
-    <div className={className}>
-      <div className="input-shadow">
-        <input type={type} onFocus={onFocus} onBlur={onBlur} />
-      </div>
-      {errorMarkup}
-    </div>
+    <UIDConsumer>
+      {(id) => (
+        <div className={className}>
+          {label && <label htmlFor={id}>{label}</label>}
+          <div className="input-shadow">
+            <input type={type} id={id} onFocus={onFocus} onBlur={onBlur} />
+          </div>
+          {errorMarkup}
+        </div>
+      )}
+    </UIDConsumer>
   );
 };
 
 TextField.propTypes = {
   /** Error to display beneath the label */
   error: PropTypes.string,
+  /** Label for the input */
+  label: PropTypes.string,
   /** Determines the type of input */
   type: PropTypes.oneOf(["text"]),
 };
