03818a782dbc6e27e8bf06f22fb84940f902c613
Kamal Mahyuddin <kamal@envoy.com>
2020-04-27 12:27:52 -0700

    Set up eslint
    Integrates with prettier

---
+++ .eslintrc.js
@@ -0,0 +1,39 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    "plugin:react-hooks/recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  plugins: [],
+  rules: {
+    "react/react-in-jsx-scope": "off", // we use babel-plugin-react-require which injects the import React anyway
+  },
+  ignorePatterns: [
+    "node_modules/",
+    "storybook-static/",
+    "index.umd.js",
+    "index.esm.js",
+  ],
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
+};
