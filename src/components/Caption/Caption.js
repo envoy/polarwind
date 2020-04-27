30ec70af02734a50e07dea85b294676a32eb99cd
Kamal Mahyuddin <kamal@envoy.com>
2020-04-26 17:35:38 -0700

    Add stories for Caption. Fixes #18

--- src/components/Caption/Caption.js
+++ src/components/Caption/Caption.js
@@ -1,10 +1,16 @@
 import PropTypes from "prop-types";
 import styles from "./Caption.module.css";
 
+/**
+ * Caption text size is smaller than the recommended size for general reading. It should
+ * be used only in an text field help text, a graph label or as a timestamp for a list
+ * item.
+ */
 export const Caption = ({ children }) => {
   return <p className={styles.Caption}>{children}</p>;
 };
 
 Caption.propTypes = {
+  /** The content to use as a text field help text, graph label or timestamp */
   children: PropTypes.node,
 };
