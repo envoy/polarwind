628e3dcf2c4ae9f2c95d33910a649981d0883548
Kamal Mahyuddin <kamal@envoy.com>
2020-04-28 12:19:52 -0700

    Remove deprecated way to configure storybook webpack

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,4 +1,30 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.(js|mdx)"],
   addons: ["@storybook/addon-docs", "@storybook/addon-a11y"],
+  webpackFinal: (config) => {
+    // Remove the existing css rule
+    config.module.rules = config.module.rules.filter(
+      (f) => f.test.toString() !== "/\\.css$/"
+    );
+
+    config.module.rules.push({
+      test: /\.css$/,
+      use: [
+        "style-loader",
+        {
+          loader: "css-loader",
+          options: {
+            modules: true,
+            importLoaders: 1,
+          },
+        },
+        "postcss-loader",
+      ],
+      include: path.resolve(__dirname, "../"),
+    });
+
+    return config;
+  },
 };
