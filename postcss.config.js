3773cbbf35150da9e17580f14a4ace83400b68fe
Kamal Mahyuddin <kamal@envoy.com>
2020-04-28 12:07:17 -0700

    Convert to the interoperable object-based format
    https://nextjs.org/docs/advanced-features/customizing-postcss-config

--- postcss.config.js
+++ postcss.config.js
@@ -1,25 +1,30 @@
-const plugins = [];
+let plugins = {};
 
 if (!process.env.PURGE && !process.env.MINIFY) {
-  plugins.push(
-    require("postcss-preset-env")({ stage: 0, browsers: "last 2 versions" }),
-    require("postcss-import"),
-    require("postcss-nested"),
-    require("tailwindcss")
-  );
+  plugins = {
+    ...plugins,
+    "postcss-preset-env": { stage: 0, browsers: "last 2 versions" },
+    "postcss-import": {},
+    "postcss-nested": {},
+    tailwindcss: {},
+  };
 }
 
 if (process.env.PURGE) {
-  plugins.push(
-    require("@fullhuman/postcss-purgecss")({
+  plugins = {
+    ...plugins,
+    "@fullhuman/postcss-purgecss": {
       content: ["./build/index.esm.js"],
       defaultExtractor: (content) => content.match(/[\w-/:]+(?<!:)/g) || [],
-    })
-  );
+    },
+  };
 }
 
 if (process.env.MINIFY) {
-  plugins.push(require("cssnano"));
+  plugins = {
+    ...plugins,
+    cssnano: {},
+  };
 }
 
 module.exports = { plugins };
