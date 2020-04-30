74d4f4010d800d46e4115d6a60172d01c0491dca
Kamal Mahyuddin <kamal@envoy.com>
2020-04-30 11:16:47 -0700

    Support onFocus and onBlur

--- src/components/Checkbox/Checkbox.js
+++ src/components/Checkbox/Checkbox.js
@@ -10,9 +10,24 @@ const cx = classnames.bind(styles);
  * Use in forms to toggle the state of something on or off. Default checkboxes can appear
  * in two states: selected and disabled, or unselected.
  */
-export const Checkbox = ({ checked, disabled, label, onChange }) => {
+export const Checkbox = ({
+  checked,
+  disabled,
+  label,
+  onBlur,
+  onChange,
+  onFocus,
+}) => {
   const handleOnChange = (event) => {
     onChange && onChange(event.currentTarget.checked);
+  };
+
+  const handleOnBlur = () => {
+    onBlur && onBlur();
+  };
+
+  const handleOnFocus = () => {
+    onFocus && onFocus();
   };
 
   const isIndeterminate = checked === "indeterminate";
@@ -27,13 +42,15 @@ export const Checkbox = ({ checked, disabled, label, onChange }) => {
   const className = cx("form-checkbox", "Checkbox");
 
   return (
-    <Label label={label} className={styles.Label}>
+    <Label className={styles.Label} label={label}>
       <input
-        type="checkbox"
         className={className}
-        onChange={handleOnChange}
-        ref={ref}
         disabled={disabled}
+        ref={ref}
+        type="checkbox"
+        onBlur={handleOnBlur}
+        onChange={handleOnChange}
+        onFocus={handleOnFocus}
       />
     </Label>
   );
@@ -46,6 +63,10 @@ Checkbox.propTypes = {
   disabled: PropTypes.bool,
   /** Label for the checkbox */
   label: PropTypes.node.isRequired,
+  /** Callback when focus is removed */
+  onBlur: PropTypes.func,
   /** Callback when checkbox is toggled */
   onChange: PropTypes.func,
+  /** Callback when checkbox is focused */
+  onFocus: PropTypes.func,
 };
