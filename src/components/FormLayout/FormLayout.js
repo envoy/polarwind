c19f3e69adfd1bcbe34602fbe74fadc628be7d66
Kamal Mahyuddin <kamal@envoy.com>
2020-05-05 18:26:07 -0700

    Finish form layout vertical spacnig

--- src/components/FormLayout/FormLayout.js
+++ src/components/FormLayout/FormLayout.js
@@ -1,5 +1,6 @@
 import classnames from "classnames/bind";
 import PropTypes from "prop-types";
+import { Item } from "./components";
 import styles from "./FormLayout.module.css";
 
 const cx = classnames.bind(styles);
@@ -12,10 +13,18 @@ export const FormLayout = ({ children }) => {
   const className = cx({
     FormLayout: true,
   });
-  return <div className={className}>{children}</div>;
+  return (
+    <div className={className}>
+      {children.map((child, i) => (
+        <Item key={i}>{child}</Item>
+      ))}
+    </div>
+  );
 };
 
 FormLayout.propTypes = {
   /** The content to display inside the layout */
   children: PropTypes.node,
 };
+
+FormLayout.Item = Item;
