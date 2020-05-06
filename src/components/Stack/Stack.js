dab7a2a7a89b2ba041f8e556c0089763c09a4dd5
Kamal Mahyuddin <kamal@envoy.com>
2020-05-06 15:17:53 -0700

    Add Stack

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -1,5 +1,8 @@
 import classnames from "classnames/bind";
 import PropTypes from "prop-types";
+import { Children } from "react";
+import { wrapWithComponent } from "../../utils/components";
+import { Item } from "./components";
 import styles from "./Stack.module.css";
 
 const cx = classnames.bind(styles);
@@ -9,14 +12,20 @@ const cx = classnames.bind(styles);
  * A stack is made of flexible items that wrap each of the stack’s children. Options
  * provide control of the wrapping, spacing, and relative size of the items in the stack.
  */
-export const Stack = ({ children }) => {
-  const className = cx({
-    Stack: true,
-  });
-  return <div className={className}>{children}</div>;
+export const Stack = ({ children, distribution }) => {
+  const className = cx("Stack", distribution);
+  return (
+    <div className={className}>
+      {Children.map(children, wrapWithComponent(Item))}
+    </div>
+  );
 };
 
 Stack.propTypes = {
   /** Elements to display inside stack */
   children: PropTypes.node,
+  /** Adjust horizontal alignment of elements */
+  distribution: PropTypes.oneOf(["equalSpacing"]),
 };
+
+Stack.Item = Item;
