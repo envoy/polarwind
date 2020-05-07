06cf7af6a09db234e2fd887d2a85cdec066e70cf
Kamal Mahyuddin <kamal@envoy.com>
2020-05-06 17:57:07 -0700

    Remove unused imports

--- src/components/Tabs/Tabs.js
+++ src/components/Tabs/Tabs.js
@@ -1,7 +1,5 @@
 import classnames from "classnames/bind";
 import PropTypes from "prop-types";
-import { Children } from "react";
-import { wrapWithComponent } from "../../utils/components";
 import { Tab } from "./components";
 import styles from "./Tabs.module.css";
 
