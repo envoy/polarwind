d6d494cfe90735260c960f136b026774e4477c0a
Kamal Mahyuddin <kamal@envoy.com>
2020-05-11 19:58:50 -0700

    Call iframeResizerContentWindow after AppProvider has been mounted
    Set defaults

--- src/components/AppProvider/AppProvider.js
+++ src/components/AppProvider/AppProvider.js
@@ -1,13 +1,23 @@
 import PropTypes from "prop-types";
+import { useEffect } from "react";
+import { iframeResizerContentWindow } from "../../vendor/iframeResizer.contentWindow";
 
 /**
  * App provider is a required component that enables sharing global settings throughout the hierarchy of your application.
  */
-export const AppProvider = ({ children }) => {
+export const AppProvider = ({
+  children,
+  origin = "https://dashboard.envoy.com",
+}) => {
+  useEffect(() => {
+    iframeResizerContentWindow({ targetOrigin: origin });
+  });
   return children;
 };
 
 AppProvider.propTypes = {
   /** Inner content of the application */
   children: PropTypes.node,
+  /** Envoy Dashboard origin */
+  origin: PropTypes.string,
 };
