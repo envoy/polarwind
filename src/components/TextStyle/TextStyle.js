b7cce1ce0871b14234c83b7f821ba69d00c25f3c
Kamal Mahyuddin <kamal@envoy.com>
2020-05-17 18:22:33 -0700

    Annotate types
    Annotate

--- src/components/TextStyle/TextStyle.js
+++ src/components/TextStyle/TextStyle.js
@@ -1,6 +1,14 @@
+// @flow
 import classNames from "classnames/bind";
-import PropTypes from "prop-types";
+import * as React from "react";
 import styles from "./TextStyle.module.css";
+
+type Props = {
+  /** The content that should get the intended styling */
+  children?: React.Node,
+  /** Give text additional visual meaning */
+  variation?: "subdued" | "positive" | "negative" | "warning",
+};
 
 const cx = classNames.bind(styles);
 
@@ -8,14 +16,10 @@ const cx = classNames.bind(styles);
  * Text style enhances text with additional visual meaning. For example, using subdued
  * text to de-emphasize it from its surrounding text.
  */
-export const TextStyle = ({ children, variation }) => {
+export const TextStyle = ({
+  children,
+  variation,
+}: Props): React.Element<any> => {
   const className = cx(variation);
   return <span className={className}>{children}</span>;
-};
-
-TextStyle.propTypes = {
-  /** The content that should get the intended styling */
-  children: PropTypes.node,
-  /** Give text additional visual meaning */
-  variation: PropTypes.oneOf(["subdued", "positive", "negative", "warning"]),
 };
