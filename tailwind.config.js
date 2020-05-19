13445f9edf66a7b75c17b1f0b667702882e3221d
Kamal Mahyuddin <kamal@envoy.com>
2020-05-19 12:25:36 -0700

    Updated focus ring width to 2px

--- tailwind.config.js
+++ tailwind.config.js
@@ -81,7 +81,7 @@ module.exports = {
       none: "none",
       default: "0px 2px 6px rgba(0, 0, 0, 0.48)",
       outline: `0 0 0 3px ${rgba(theme("colors.carbon.20%"), 0.2)}`,
-      "outline-red": `0 0 0 3px ${rgba(theme("colors.brand.default"), 0.3)}`,
+      "outline-brand": `0 0 0 2px ${rgba(theme("colors.brand.default"), 0.3)}`,
       "input-pacific": [
         `0 0 0 1px ${theme("colors.pacific.default")}`,
         `0 1px 4px 0 ${rgba(theme("colors.pacific.default"), 0.16)}`,
