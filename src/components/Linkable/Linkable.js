465fcd13b61bef0627fac0790475e5fe43da72df
Kamal Mahyuddin <kamal@envoy.com>
2020-05-19 11:43:35 -0700

    Add an internal Linkable component

---
+++ src/components/Linkable/Linkable.js
@@ -0,0 +1,22 @@
+import PropTypes from "prop-types";
+import { UnstyledLink } from "../UnstyledLink";
+
+/**
+ * Internal component to choose between a UnstyledLink or button
+ */
+export const Linkable = ({ children, url, ...rest }) => {
+  return url ? (
+    <UnstyledLink {...rest} url={url}>
+      {children}
+    </UnstyledLink>
+  ) : (
+    <button {...rest}>{children}</button>
+  );
+};
+
+Linkable.propTypes = {
+  /** The content to display inside the link */
+  children: PropTypes.node,
+  /** The url to link to */
+  url: PropTypes.string,
+};
---
+++ src/components/Linkable/tests/Linkable.test.js
@@ -0,0 +1,12 @@
+import { render, screen } from "@testing-library/react";
+import { Linkable } from "../Linkable";
+
+test("when url prop is present", () => {
+  render(<Linkable url="/about">About</Linkable>);
+  expect(screen.getByText("About")).toContainHTML('<a href="/about">About</a>');
+});
+
+test("when url prop is missing", () => {
+  render(<Linkable>About</Linkable>);
+  expect(screen.getByText("About")).toContainHTML("<button>About</button>");
+});
