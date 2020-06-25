6f84ac84c7d4e0940b14bf4df837b513c27e2c7a
Kamal Mahyuddin <kamal@envoy.com>
2020-06-25 13:25:59 -0700

    Don't need the custom frame anymore since we import src

--- playroom/FrameComponent.js
+++
@@ -1,9 +0,0 @@
-import { AppProvider } from "../src/components";
-import "../src/styles/common.css";
-
-// eslint-disable-next-line react/prop-types
-const FrameComponent = ({ children }) => (
-  <AppProvider embedded={false}>{children}</AppProvider>
-);
-
-export default FrameComponent;
