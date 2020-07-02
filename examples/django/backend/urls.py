283020f0c88a9c6e02b93886b2c12c030cc792f4
Kamal Mahyuddin <kamal@envoy.com>
2020-07-02 13:02:06 -0700

    Wire up skeleton

--- examples/django/backend/urls.py
+++ examples/django/backend/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path
+from django.urls import path, re_path, include
 from django.conf import settings
 from cra_helper.views import proxy_cra_requests
 
@@ -22,6 +22,7 @@ from .views import HomeView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
+    path('auth/', include('envoy_auth.urls')),
     path('admin/', admin.site.urls),
 ]
 
--- examples/django/envoy_auth/views.py
+++ examples/django/envoy_auth/views.py
@@ -1,3 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from authlib.integrations.django_client import OAuth
 
-# Create your views here.
+oauth = OAuth()
+oauth.register(
+    name='envoy',
+    client_kwargs={
+        'scope': 'public'
+    }
+)
+
+
+def login(request):
+    redirect_uri = request.build_absolute_uri(reverse('authorize'))
+    return oauth.envoy.authorize_redirect(request, redirect_uri)
+
+def authorize(request):
+    token = oauth.envoy.authorize_access_token(request)
+    request.session['user'] = { "token": token }
+    return redirect('/')
