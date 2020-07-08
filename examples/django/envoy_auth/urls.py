5c7dc9a25bca30fec7e6a253b10f772b8d786ab2
Kamal Mahyuddin <kamal@envoy.com>
2020-07-07 20:17:41 -0700

    Fix indentation

--- examples/django/envoy_auth/urls.py
+++ examples/django/envoy_auth/urls.py
@@ -3,6 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-  path('', views.login),
-  path('/callback', views.authorize, name='authorize')
+    path('', views.login),
+    path('/callback', views.authorize, name='authorize'),
 ]
