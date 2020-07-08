e84b20d15413f18fe58cb43c41be2e9ffb3b64fc
Kamal Mahyuddin <kamal@envoy.com>
2020-07-07 18:55:17 -0700

    Fix indentation

--- examples/django/backend/views.py
+++ examples/django/backend/views.py
@@ -5,10 +5,10 @@ class HomeView(TemplateView):
     template_name = "home.html"
 
     def get_context_data(self, **kwargs):
-            context = super().get_context_data(**kwargs)
-            # set any other context for the react app here like the root component to use
-            # context["component"] = "Foo" (defaults to "App")
+        context = super().get_context_data(**kwargs)
+        # set any other context for the react app here like the root component to use
+        # context["component"] = "Foo" (defaults to "App")
 
-            # and any dynamic props to set on the component
-            # context["props"] = { "foo": "bar" }
-            return context
+        # and any dynamic props to set on the component
+        # context["props"] = { "foo": "bar" }
+        return context
