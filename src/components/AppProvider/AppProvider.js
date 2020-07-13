09aaff76ae40902c1f70a04338fadb6cde2aab66
Kamal Mahyuddin <kamal@envoy.com>
2020-07-13 12:55:54 -0700

    Cautiously optimistic this is working

--- src/components/AppProvider/AppProvider.js
+++ src/components/AppProvider/AppProvider.js
@@ -1,9 +1,7 @@
 import PropTypes from "prop-types";
-import { useEffect } from "react";
 import { EmbeddedContext } from "../../utils/embedded";
 import { OriginContext } from "../../utils/origin";
-import { useParent } from "../../utils/parent";
-import { iframeResizerContentWindow } from "../../vendor/iframeResizer.contentWindow";
+import { ParentProvider } from "../../utils/parent";
 
 /**
  * App provider is a required component that enables sharing global settings throughout the hierarchy of your application.
@@ -13,24 +11,11 @@ export const AppProvider = ({
   embedded = true,
   origin = "https://dashboard.envoy.com",
 }) => {
-  const [, receiveMessage, setParent] = useParent();
-
-  useEffect(() => {
-    iframeResizerContentWindow({
-      onMessage: receiveMessage,
-      onReady: () => {
-        setParent(window.parentIFrame);
-        // can't use sendMessage here because it looks like it needs a tick before
-        // parent.current has the iframe
-        window.parentIFrame.sendMessage({ event: "ready" });
-      },
-      targetOrigin: origin,
-    });
-  }, [origin, receiveMessage, setParent]);
-
   return (
     <EmbeddedContext.Provider value={embedded}>
-      <OriginContext.Provider value={origin}>{children}</OriginContext.Provider>
+      <OriginContext.Provider value={origin}>
+        <ParentProvider>{children}</ParentProvider>
+      </OriginContext.Provider>
     </EmbeddedContext.Provider>
   );
 };
--- src/utils/parent.js
+++ src/utils/parent.js
@@ -1,7 +1,16 @@
-import { createContext, createRef, useContext } from "react";
+import PropTypes from "prop-types";
+import {
+  createContext,
+  createRef,
+  useContext,
+  useEffect,
+  useState,
+} from "react";
+import { iframeResizerContentWindow } from "../vendor/iframeResizer.contentWindow";
+import { OriginContext } from "./origin";
 
 const parent = createRef();
-const ParentContext = createContext();
+const ParentContext = createContext({ default: "what" });
 
 function setParent(value) {
   console.log("[polarwind] setParent", { value });
@@ -16,11 +25,68 @@ function sendMessage(message) {
 function receiveMessage(message) {
   switch (message.event) {
     case "context": {
-      console.log("[polarwind] received context", { context: message.context });
+      console.log("[polarwind] received context", {
+        context: message.context,
+      });
+      const action = { payload: message.context, type: "context" };
+      console.log("[polarwind] dispatching", { action });
       break;
     }
   }
 }
+
+// try additive context
+// function reducer(state, action) {
+//   console.log("[polarwind] reducer called", { action, state });
+//   switch (action.type) {
+//     case "context": {
+//       return {
+//         ...state,
+//         ...action.payload,
+//       };
+//     }
+//   }
+// }
+function ParentProvider({ children }) {
+  const [, setParent] = useParent();
+  const origin = useContext(OriginContext);
+  const [context, setContext] = useState({});
+
+  useEffect(() => {
+    console.log("[polaris] iframeResizerContentWindow useEffect called", {
+      origin,
+      setContext,
+      setParent,
+    });
+    iframeResizerContentWindow({
+      onMessage: (message) => {
+        console.log("[polaris] onMessage", { message });
+        switch (message.event) {
+          case "context": {
+            setContext(message.context);
+            break;
+          }
+        }
+      },
+      onReady: () => {
+        setParent(window.parentIFrame);
+        // can't use sendMessage here because it looks like it needs a tick before
+        // parent.current has the iframe
+        window.parentIFrame.sendMessage({ event: "ready" });
+      },
+      targetOrigin: origin,
+    });
+  }, [origin, setParent]);
+
+  return (
+    <ParentContext.Provider value={context}>{children}</ParentContext.Provider>
+  );
+}
+
+ParentProvider.propTypes = {
+  /** Inner content of the application */
+  children: PropTypes.node,
+};
 
 function useParent() {
   return [sendMessage, receiveMessage, setParent];
@@ -30,4 +96,4 @@ function useParentContext() {
   return useContext(ParentContext);
 }
 
-export { useParent, useParentContext };
+export { useParent, useParentContext, ParentProvider };
