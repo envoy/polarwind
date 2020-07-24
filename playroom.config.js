137582467dfd0db46553a8b5c0c85b90cc72e61d
Kamal Mahyuddin <kamal@envoy.com>
2020-07-23 17:37:20 -0700

    Add config to load svg

--- playroom.config.js
+++ playroom.config.js
@@ -47,6 +47,11 @@ module.exports = {
             },
           ],
         },
+        {
+          test: /\.svg$/,
+          exclude: /node_modules/,
+          use: "url-loader",
+        },
       ],
     },
   }),
