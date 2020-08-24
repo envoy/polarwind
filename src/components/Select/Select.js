3f6ed9b942b2172f24a2e4980388421f15a034f8
Kamal Mahyuddin <kamal@envoy.com>
2020-08-24 14:46:31 -0700

    wip

--- src/components/Select/Select.js
+++ src/components/Select/Select.js
@@ -1,9 +1,135 @@
+import { useButton } from "@react-aria/button";
+import { FocusScope } from "@react-aria/focus";
+import { useFocus } from "@react-aria/interactions";
+import { useListBox, useOption } from "@react-aria/listbox";
+import { DismissButton, useOverlay } from "@react-aria/overlays";
+import { HiddenSelect, useSelect } from "@react-aria/select";
+import { mergeProps } from "@react-aria/utils";
+import { Item } from "@react-stately/collections";
+import { useSelectState } from "@react-stately/select";
 import classnames from "classnames/bind";
 import PropTypes from "prop-types";
+import { useRef, useState } from "react";
 import { Labeled } from "../Labeled";
 import styles from "./Select.module.css";
 
 const cx = classnames.bind(styles);
+
+/* eslint-disable react/prop-types */
+function CustomSelect({
+  children,
+  disabled,
+  error,
+  helpText,
+  label,
+  labelHidden,
+  required,
+  success,
+  value,
+  ...otherProps
+}) {
+  const props = {
+    ...otherProps,
+    children,
+    defaultSelectedKey: value,
+    isDisabled: disabled,
+    isRequired: required,
+    label,
+  };
+  const state = useSelectState(props);
+  const ref = useRef();
+  const { labelProps, menuProps, triggerProps, valueProps } = useSelect(
+    props,
+    state,
+    ref
+  );
+
+  const { buttonProps } = useButton(triggerProps, ref);
+
+  return (
+    <Labeled
+      {...labelProps}
+      className={styles.Label}
+      error={error}
+      helpText={helpText}
+      hidden={labelHidden}
+      label={label}
+      required={required}
+      success={success}
+    >
+      <HiddenSelect
+        isDisabled={disabled}
+        label={label}
+        state={state}
+        triggerRef={ref}
+      />
+      <button {...buttonProps} className="form-select" ref={ref}>
+        <span {...valueProps}>{state.selectedItem?.rendered}</span>
+      </button>
+      {state.isOpen && <ListBoxPopup {...menuProps} state={state} />}
+    </Labeled>
+  );
+}
+
+function ListBoxPopup({ state, ...otherProps }) {
+  const ref = useRef();
+  const { listBoxProps } = useListBox(
+    {
+      autoFocus: state.focusStrategy,
+    },
+    state,
+    ref
+  );
+
+  // overlay
+  const overlayRef = useRef();
+  const { overlayProps } = useOverlay({
+    isDismissable: true,
+    isOpen: state.isOpen,
+    onClose: () => state.close(),
+    shouldCloseOnBlur: true,
+  });
+
+  return (
+    <FocusScope restoreFocus>
+      <div {...overlayProps} ref={overlayRef}>
+        <DismissButton onDismiss={() => state.close()} />
+        <ul {...mergeProps(listBoxProps, otherProps)} ref={ref}>
+          {[...state.collection].map((item) => {
+            return <Option item={item} key={item.key} state={state} />;
+          })}
+        </ul>
+        <DismissButton onDismiss={() => state.close()} />
+      </div>
+    </FocusScope>
+  );
+}
+
+function Option({ item, state }) {
+  const ref = useRef();
+  const isDisabled = state.disabledKeys.has(item.key);
+  const isSelected = state.selectionManager.isSelected(item.key);
+  const { optionProps } = useOption(
+    {
+      isDisabled,
+      isSelected,
+      key: item.key,
+      shouldFocusOnHover: true,
+      shouldSelectOnPressUp: true,
+    },
+    state,
+    ref
+  );
+  const [, setFocused] = useState(false);
+  const { focusProps } = useFocus({ onFocusChange: setFocused });
+
+  return (
+    <li {...mergeProps(optionProps, focusProps)} ref={ref}>
+      {item.rendered}
+    </li>
+  );
+}
+/* eslint-enable */
 
 function renderOption(option) {
   const { disabled, label, options, title, value } = option;
@@ -35,7 +161,7 @@ function renderOption(option) {
  * Select lets users choose one option from an options menu. Consider select when you have
  * 4 or more options, to avoid cluttering the interface.
  */
-export const Select = ({
+const Select = ({
   disabled,
   error,
   helpText,
@@ -140,3 +266,5 @@ Select.propTypes = {
   /** Initial value for the input */
   value: PropTypes.string,
 };
+
+export { Select, CustomSelect, Item };
