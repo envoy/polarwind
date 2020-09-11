cf565072865e2ca91bdf84d5e9227f8fd5ed151b
Kamal Mahyuddin <kamal@envoy.com>
2020-09-11 12:14:05 -0700

    Add banner component

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,3 +1,6 @@
 import DeliveriesEmptyState from "./deliveries-empty-state.svg";
+import Info from "./info.svg";
+import Success from "./success.svg";
+import Warning from "./warning.svg";
 
-export { DeliveriesEmptyState };
+export { DeliveriesEmptyState, Info, Success, Warning };
---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,46 @@
+import classnames from "classnames/bind";
+import PropTypes from "prop-types";
+import { Info, Success, Warning } from "../../assets";
+import { Heading } from "../Heading";
+import { Stack } from "../Stack";
+import styles from "./Banner.module.css";
+
+const cx = classnames.bind(styles);
+
+const icons = {
+  info: Info,
+  success: Success,
+  warning: Warning,
+};
+
+/**
+ * Informs users about important changes or persistent conditions. Use this component
+ * if you need to communicate to users in a prominent way. Banners are placed at the
+ * top of the page or section they apply to, and below the page or section header.
+ */
+export const Banner = ({ children, status, title }) => {
+  const className = cx("Banner", status);
+
+  const titleMarkup = title && <Heading size="small">{title}</Heading>;
+
+  return (
+    <div className={className}>
+      <span className={styles.Icon}>
+        <img alt={`${status} icon`} src={icons[status]} />
+      </span>
+      <Stack vertical>
+        {titleMarkup}
+        <div className={styles.Body}>{children}</div>
+      </Stack>
+    </div>
+  );
+};
+
+Banner.propTypes = {
+  /** The child elements to render in the banner. */
+  children: PropTypes.node,
+  /** Sets the status of the banner. */
+  status: PropTypes.oneOf(["success", "info", "warning"]).isRequired,
+  /** Title content for the banner. */
+  title: PropTypes.string,
+};
