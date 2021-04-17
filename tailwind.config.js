77f31b316ac66714a56d4bed0cda60c0ebd325da
Kamal Mahyuddin <kamal@envoy.com>
2021-04-16 17:42:44 -0700

    Remove values that have been moved to the preset

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,4 @@
-const preset = require("@envoy/tailwind");
 const hexRgb = require("hex-rgb");
-const resolveConfig = require("tailwindcss/resolveConfig");
-
-const { theme } = resolveConfig(preset);
 
 function rgba(hex, alpha) {
   const { blue, green, red } = hexRgb(hex);
@@ -10,7 +6,7 @@ function rgba(hex, alpha) {
 }
 
 module.exports = {
-  presets: [preset],
+  presets: [require("@envoy/tailwind")],
   theme: {
     // it's hard to extend boxShadow since it needs the complete string including color,
     // which is why the best place to define it is by overriding the entire boxShadow
@@ -24,35 +20,10 @@ module.exports = {
       outline: `0 0 0 2px ${theme("colors.carbon.10")}`,
       "outline-red-50": `0 0 0 2px ${rgba(theme("colors.red.50"), 0.3)}`,
       "outline-toggle": `0 0 0 2px ${theme("colors.carbon.20")}`,
-      "input-gem": [
-        `0 0 0 1px ${theme("colors.gem.50")}`,
-        `0 1px 4px 0 ${rgba(theme("colors.gem.50"), 0.16)}`,
-      ].join(),
-      "input-papaya": [
-        `0 0 0 1px ${theme("colors.papaya.50")}`,
-        `0 1px 4px 0 ${rgba(theme("colors.papaya.50"), 0.16)}`,
-      ].join(),
-      "input-blur-papaya": `0 0 0 1px ${theme("colors.papaya.50")}`,
-      "checkbox-inset": [
-        "inset 0px 1px 1.5px rgba(0, 0, 0, 0.26)",
-        "inset 0px -1px 1.5px rgba(0, 0, 0, 0.1)",
-      ].join(),
       toggle: "0px 2px 6px rgba(0, 0, 0, 0.48)",
     }),
     extend: {
-      colors: {
-        carbon: {
-          // TODO: tailwind 2.x will automatically perform deep merges so it won't be
-          // necessary to spread the previous values anymore
-          ...theme.colors.carbon,
-          checkbox: "#b4b4b4",
-        },
-      },
-      borderRadius: {
-        checkbox: "0.1875rem",
-      },
       borderWidth: {
-        checkbox: "0.5px",
         6: "6px",
       },
       flex: {
